40fb897c3fb686bf241e1297787574fefcd9795e
Alex Lei <alexlei@pc-143-78.customer.ask4.lan>
2023-09-29 16:27:33 +0100

    Revert filename to helloworld.java

--- practical2/helloworld.java
+++ practical2/helloworld.java
@@ -1,6 +1,7 @@
-public class helloworld{
-    public static void(String args){
-        System.out.println("Helloworld!");
+public class HelloWorld {
 
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
     }
 }
